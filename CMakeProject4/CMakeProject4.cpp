2649ec9c08c5fb472d2fb4eb6f9e24f4a9832d8d
maximcheg <max.cheglakovvv@mail.ru>
2023-06-24 11:21:46 +0300

    second commit add swap

--- CMakeProject4/CMakeProject4.cpp
+++ CMakeProject4/CMakeProject4.cpp
@@ -3,10 +3,23 @@
 
 #include "CMakeProject4.h"
 
-using namespace std;
-
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+template <typename T>
+T swap(T x, T y) {
+	T* z = new T;
+	z = x;
+	x = y;
+	y = z;
+	delete z;
 }
+
+int main() {
+	std::string x = "first";
+	std::string y = "second";
+
+	std::cout << x << ", " << y << '\n';
+
+	swap(x, y);
+
+	std::cout << x << ", " << y << '\n';
+}
+
